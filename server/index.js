950a8a3ced9c69a62c2823ec1e436cdf825be3e5
unknown <federicoreinavinchira@yahoo.com>
2020-09-26 08:32:36 -0500

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+
+const express = require("express");
+var cors = require('cors');
+const app = express();
+var http = require('http');
+
+var PORT = process.env.PORT || 5000;
+var path = require('path');
+
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use(require("./routes/main.routes"));
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
+var server = http.createServer(app);
+server.listen(PORT)
+
---
+++ server/routes/main.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const express = require("express");
+const router = express.Router();
+
+router.post("/confirmar",(req,res) =>{
+    res.end("<h1>Sergio gay</h1>")
+})
+
+router.use(express.static())
+module.exports = router;
