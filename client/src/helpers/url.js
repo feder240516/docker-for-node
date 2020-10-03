23beb2d47761b76f9f22b97117319ffb68421123
Federico Reina <federicoreinavinchira@yahoo.com>
2020-10-03 04:30:53 -0500

    local url

--- client/src/helpers/url.js
+++ client/src/helpers/url.js
@@ -2,5 +2,5 @@
 
 export default function getUrl(){
     if (process.env.REACT_APP_PLACE === 'local')return 'http://localhost:8080'
-    else return 'http://104.214.112.160';
+    else return 'http://10.3.2.4';
 }
